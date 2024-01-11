c9bcda38732cc15e72eec3c8319ee32a72ffd141
Neha Thakor <neha.k.thakor@gmail.com>
2024-01-11 15:50:28 +0530

    second commit

--- Collection/ArrLists.java
+++ Collection/ArrLists.java
@@ -1,6 +1,6 @@
 package DSA.Collection;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class ArrLists {
     public static void main(String[] args) {
@@ -9,22 +9,67 @@ public class ArrLists {
         studentName.add("Deepika");
         studentName.add("Rachit");
 
-        //To add element in the arraylist at any index
+        //To add element in the arraylist at any index || T.C - O(n)
         studentName.add(1,"Riya");
         System.out.println(studentName);
 
-        ArrayList<String> newlist  = new ArrayList<>();
+        ArrayList<String> newlist  = new ArrayList<>();  
         newlist.add("N");
         newlist.add("R");
         newlist.add("D");
 
-        //To merge two arraylist
+        //To merge two arraylist || T.C - O(n)
         newlist.addAll(studentName);
         System.out.println(newlist);
+
+        //to get indexed element || T.C - O(1)
+        System.out.println(newlist.get(6));
+
+        //to remove element by index position.. also note that u can't specify directly in sout || T.C - O(n)
+        newlist.remove(6);
+        System.out.println(newlist);
+
+        //to remove element by object || T.C - O(n)
+        newlist.remove(String.valueOf("Riya"));
+        System.out.println(newlist);
+        /* 
+        //to remove list from a list
+        newlist.removeAll(studentName);
+        System.out.println(newlist);
+        */
+
+        /* 
+        //clear complete list
+        newlist.clear();
+        System.out.println(newlist);
+        */
+
+        //to set new element at a predefined index
+        newlist.set(1,"T");
+        System.out.println(newlist);
+ 
+        //to check if an element do exists or not || T.C - O(n)
+        System.out.println(newlist.contains("Neha"));
+        /* 
+        //to check if a list exists in another list
+        System.out.println(newlist.containsAll(studentName));
+        */
+
+        //Looping in the list:
+        for (int i = 0; i < newlist.size(); i++) {
+            System.out.println(newlist.get(i));
+        }
+
+        for(String str : newlist){  //recommended
+            System.out.println(str);
+        }
+
+        Iterator<String> it = newlist.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
     }
 }
-
-
 
 /*
  Java ArrayList is a part of the Java collection framework and it is a class of java.util package.
