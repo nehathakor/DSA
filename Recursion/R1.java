64f77983e7c77ffed98775e80208f379bbd485fa
Neha Thakor <neha.k.thakor@gmail.com>
2024-02-02 15:13:07 +0530

    second commit

---
+++ Recursion/R1.java
@@ -0,0 +1,18 @@
+//Print name n times
+//time-complexity: O(n)|| space-complexity: O(n)
+
+package DSA.Recursion;
+
+public class R1 {
+    static void F(int i ,int n){
+        if(i > n){
+            return;
+        }
+        System.out.println("Neha");
+        F(i+1, n);
+    }
+
+    public static void main(String[] args) {
+        F(1,5);
+    }
+}
