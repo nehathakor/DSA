ad2be1e340fde9b893d11e32649d7f66ed098646
Neha Thakor <neha.k.thakor@gmail.com>
2024-02-03 18:13:16 +0530

    second commit

--- Recursion/Sum.java
+++ Recursion/Sum.java
@@ -1,6 +1,7 @@
 package DSA.Recursion;
 
 public class Sum {
+    //Parameterised Recursion:
     static void F(int i, int sum){
         if(i < 1){
             System.out.println(sum);
@@ -12,4 +13,19 @@ public class Sum {
     public static void main(String[] args) {
         F(5,0);
     }
+
+    /*
+    //Functional Recursion:
+
+    static int F(int n){
+    if(n == 0){
+        return 0;
+    }
+    return n+F(n-1);
+    }
+
+    public static void main(String[] args) {
+    System.out.println(F(3));
+    }
+     */
 }
