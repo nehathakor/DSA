d1e3f2268ec1e3baaadebc0393234a95aed86c0f
Neha Thakor <neha.k.thakor@gmail.com>
2024-02-04 16:38:25 +0530

    second commit

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,16 @@
+package DSA.Recursion;
+
+public class Fibonacci {
+    static int F(int n){
+        if(n <= 1){
+            return n;
+        }
+        F(n-1);
+        F(n-2);
+        return F(n-1)+F(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(F(4));
+    }
+}
