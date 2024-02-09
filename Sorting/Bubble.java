e7b7edbfbdd99f2d91ba73efbc829a2df4c4d882
Neha Thakor <neha.k.thakor@gmail.com>
2024-02-09 15:25:46 +0530

    second commit

---
+++ Sorting/Bubble.java
@@ -0,0 +1,27 @@
+package DSA.Sorting;
+
+import java.util.Arrays;
+
+public class Bubble {
+    static void bubbleSort(int arr[],int n){
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = 0; j <= i; j++) {
+                if(arr[j] > arr[j+1]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+
+    static void swap(int arr[],int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2,0,5,3,1};
+        bubbleSort(arr, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
