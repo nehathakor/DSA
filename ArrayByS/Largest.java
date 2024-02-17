9c15d46726e4bfb88e15a14ee8b9c8c2d077ec72
Neha Thakor <neha.k.thakor@gmail.com>
2024-02-17 14:28:47 +0530

    second commit

---
+++ ArrayByS/Largest.java
@@ -0,0 +1,17 @@
+package DSA.ArrayByS;
+
+public class Largest {
+    static int largestElement(int arr[]){
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] >= max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int arr[] = {3,5,1,3,9,88};
+        System.out.println(largestElement(arr));
+    }
+}
