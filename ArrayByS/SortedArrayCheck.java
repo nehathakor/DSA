6debb2e457a4ff6fd66ddf6596bce057475e91b6
Neha Thakor <neha.k.thakor@gmail.com>
2024-02-17 15:18:14 +0530

    second commit

--- ArrayByS/SortedArrayCheck.java
+++ ArrayByS/SortedArrayCheck.java
@@ -3,7 +3,8 @@
 package DSA.ArrayByS;
 
 public class SortedArrayCheck {
-    //non-decreasind order i.e; ascending
+    /* 
+    //non-decreasing order i.e; ascending
     static int checkSort(int arr[]){
         for (int i = 1; i < arr.length; i++) {
             if(arr[i] >= arr[i-1]){
@@ -18,6 +19,25 @@ public class SortedArrayCheck {
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
+        System.out.println(checkSort(arr));
+    }
+    */
+
+    //descending order
+    static int checkSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] <= arr[i-1]){
+
+            }
+            else{
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
         System.out.println(checkSort(arr));
     }
 }
