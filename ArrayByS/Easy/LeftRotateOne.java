dadba37ba862a14a2e0d8cc3372735ca756b6fed
Neha Thakor <neha.k.thakor@gmail.com>
2024-02-18 19:05:08 +0530

    second commit

---
+++ ArrayByS/Easy/LeftRotateOne.java
@@ -0,0 +1,22 @@
+//time-complexity:O(N) || space-complexity:O(N)
+
+package DSA.ArrayByS.Easy;
+
+import java.util.Arrays;
+
+public class LeftRotateOne {
+    static int[] LeftRotate(int arr[]){
+        int temp = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i-1] = arr[i];
+        }
+        arr[arr.length-1] = temp;
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {4,9,6,4,3,0};
+        LeftRotate(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
